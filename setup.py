68666996ebd3c70bb2bb720913239fbb18d5ea59
Marko Petrovic <mpetrovic.ks@gmail.com>
2026-09-20 23:33:20 +0100

    Bump version: 0.3.4 -> 0.3.4.post1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autoed',
-    version='0.3.4',
+    version='0.3.4.post1',
     license="BSD-3-Clause",
     entry_points={
         'console_scripts': [
